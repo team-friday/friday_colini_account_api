a5d6033de5854700f8f45c670c5ae647d7498290
Juyoung <juy5790@outlook.com>
2019-05-15 16:46:30 +0900

    update file security config

--- src/main/java/com/friday/colini/firdaycoliniaccountapi/config/SecurityConfig.java
+++ src/main/java/com/friday/colini/firdaycoliniaccountapi/config/SecurityConfig.java
@@ -1,37 +1,37 @@
 package com.friday.colini.firdaycoliniaccountapi.config;
 
-import com.friday.colini.firdaycoliniaccountapi.service.AccountService;
+import com.friday.colini.firdaycoliniaccountapi.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    AccountService accountService;
+    CustomUserDetailsService customUserDetailsService;
 
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @Bean
-    public TokenStore tokenStore(){
-        return new InMemoryTokenStore(); // todo : Change token store ( JWT or JDBC store )
-    }
-
-    @Bean
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(accountService)
+                .userDetailsService(customUserDetailsService)
                 .passwordEncoder(passwordEncoder);
     }
 
@@ -49,5 +49,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                    .and()
+                .csrf()
+                    .disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                        .antMatchers("/session/**").permitAll()
+                .anyRequest().authenticated();
 
+//      Add our custom JWT security filter
+//        http.addFilterBefore(jwtAuthenticationFilter(), FilterSecurityInterceptor.class);
+    }
+
+//    protected JwtAuthenticationFilter jwtAuthenticationFilter(){
+//        new RequestMatcher().matches("/**");
+//        return new JwtAuthenticationFilter(new AntPathMatcher("/**"));
+//    }
 }
